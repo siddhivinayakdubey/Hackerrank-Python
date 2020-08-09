949958f089e9ff4ca58182dff02251f39cca866c
Siddhivinayak Dubey <45829257+siddhivinayakdubey@users.noreply.github.com>
2020-08-09 20:02:59 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,13 @@
+import sys
+N = int(input())
+
+if N % 2 != 0:
+    print ("Weird")
+else:
+    if N >= 2 and N <= 5:
+        print ("Not Weird")
+    elif N >= 6 and N <= 20:
+        print ("Weird")
+    elif N > 20:
+        print ("Not Weird")
+
